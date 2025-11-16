a3db9ac7e9041722bcb42d77bdaec91c53de1741
Dhruv Ramachundrah <DR717@live.mdx.ac.uk>
2025-11-16 21:20:26 +0400

    Added middleware

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var cors = require('cors');
+var morgan = require('morgan');
+const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+
+// Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+
+//Middleware
+app.use(cors());
+app.use(express.json()); 
+app.use(morgan('dev'));
 
 // Sample route
 app.get('/', function(req, res) {
     res.json({ message: 'Hello, World!' });
+    res.end();
     
 });
 
-var PORT = process.env.PORT || 3000;
+
+app.get('/status', function(req, res) {
+    res.json({ status: 'Server is running smoothly!' });
+    res.end();
+});
+
 app.listen(PORT, function() {
     console.log(`Server is running on port http://localhost:${PORT}`);
 });
