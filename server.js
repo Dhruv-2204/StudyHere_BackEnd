315c5ca94c47730d56fe60a09446a0c844685dd7
Dhruv Ramachundrah <DR717@live.mdx.ac.uk>
2025-11-16 21:34:10 +0400

    Added and tested mongoDB connection

--- server.js
+++ server.js
@@ -3,7 +3,38 @@ var app = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var morgan = require('morgan');
+var dotenv = require('dotenv');
+
 const PORT = process.env.PORT || 3000;
+const {MongoClient} = require('mongodb');
+
+dotenv.config();
+
+// MongoDb Connection URI
+const uri = dotenv.config().parsed.MONGO_URI;
+const client = new MongoClient(uri);
+let db;
+
+// Connect to MongoDB
+async function connectToDB() {
+    try {
+        await client.connect();
+        db = client.db('LessonShop');
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('Failed to connect to MongoDB', err);
+    }
+};
+
+function getDB() {
+    return db;
+}
+
+// Initialize DB connection
+module.exports = { getDB };
+
+// Start the connection
+connectToDB();
 
 
 // Body Parser Middleware
@@ -21,6 +52,19 @@ app.get('/', function(req, res) {
     res.json({ message: 'Hello, World!' });
     res.end();
     
+});
+
+// load something from db to test
+app.get('/lessons', async function(req, res) {
+    try {
+        const lessonsCollection = getDB().collection('lessons');
+        const lessons = await lessonsCollection.find({}).toArray();
+        res.json(lessons);
+    } catch (err) {
+        console.error('Error fetching lessons:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.end();
 });
 
 
